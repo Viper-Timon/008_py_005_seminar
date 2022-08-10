cd793712b418fab0bfdc2751b4e6621dcb1afecc
viper-timon <viper-timon@mail.ru>
2022-08-10 14:57:29 +0300

    Tasks 2, 3 done

---
+++ Task_HW_2_x_o.py
@@ -0,0 +1,58 @@
+import random
+import os
+
+os.system('cls')
+
+a = [['*', '*', '*'],
+     ['*', '*', '*'],
+     ['*', '*', '*']]
+p_list = ['x', 'o']
+play_flag = random.randint(0, 1)
+
+print(f'первыми ходят {p_list[play_flag]}-ки')
+
+for step in range(9):
+    m, n = 0, 0
+    empt_flag=0
+
+    for i in a:
+        print(*i)
+
+    while not(0<m<4) or not(0<n<4) or not(empt_flag==1)==1:
+        m, n = map(int, input(f'Куда поставить {p_list[play_flag]}? ').split(' '))
+               
+        for i in range(len(a)):
+            for j in range(len(a[i])):
+                b = a[i]
+                if i == m-1 and j == n-1 and b[j]=='*':
+                    b[j] = p_list[play_flag]
+                    a[i] = b
+                    empt_flag=1
+            
+    os.system('cls')
+        
+    c = [k for lev_1 in a for k in lev_1]
+
+    if c[0] == c[3] == c[6] == p_list[play_flag] \
+        or c[1] == c[4] == c[7] == p_list[play_flag] \
+        or c[2] == c[5] == c[8] == p_list[play_flag] \
+        or c[0] == c[1] == c[2] == p_list[play_flag] \
+        or c[3] == c[4] == c[5] == p_list[play_flag] \
+        or c[6] == c[7] == c[8] == p_list[play_flag] \
+        or c[0] == c[4] == c[8] == p_list[play_flag] \
+        or c[2] == c[4] == c[6] == p_list[play_flag]:
+        print(f'Победили {p_list[play_flag]}-ки')
+        for i in a:
+            print(*i)
+        break
+        
+    elif step == 8:
+        print('Ничья')
+        for i in a:
+            print(*i)
+
+    
+    if play_flag==0:play_flag=1
+    else: play_flag=0
+
+    
---
+++ Task_HW_3_RLE.py
@@ -0,0 +1,63 @@
+# 3. Реализуйте RLE алгоритм:
+# реализуйте модуль сжатия и восстановления данных.
+
+def f_rle(txt):
+    txt_out = ''
+    count_rep = 1
+
+    for i in range(len(txt)):
+        if i == len(txt)-1:
+            count_rep += 0
+            txt_out += str(count_rep)+txt[i]
+        elif txt[i] == txt[i+1]:
+            count_rep += 1
+        else:
+            txt_out += str(count_rep)+txt[i]
+            count_rep = 1
+
+    count_uniq = 0
+    txt_out_comp = ''
+    txt_mult = ''
+
+    for i in range(len(txt_out)-1):
+        if txt_out[i].isdigit() == True:
+            if txt_out[i] != '1':
+                if count_uniq < 0:
+                    txt_out_comp += str(count_uniq)+txt_mult
+                    count_uniq = 0
+                    txt_mult = ''
+                txt_out_comp += txt_out[i]+txt_out[i+1]
+
+            elif i == len(txt_out)-2:
+                if count_uniq < 0:
+                    count_uniq -= 1
+                    txt_mult += txt_out[i+1]
+                    txt_out_comp += str(count_uniq)+txt_mult
+                    count_uniq = 0
+                    txt_mult = ''
+
+            elif txt_out[i] == '1':
+                count_uniq -= 1
+                txt_mult += txt_out[i+1]
+    return txt_out_comp
+
+def rle_ext(txt):
+    txt_out = ''
+    for i in range(len(txt)):
+        if txt[i] == '-':
+            dif = i+2
+            txt_out += txt[dif:dif+int(txt[i+1])]
+        else:
+            if (txt[i].isdigit()==True) \
+                and (txt[i-1]!='-'):
+                txt_out += int(txt[i])*txt[i+1]
+
+    return txt_out
+
+text_1 = 'abcabcaaaabbbcddddabcabcabcaaaaaaa'
+text_2 = f_rle(text_1)
+text_3 = rle_ext(text_2)
+
+print(text_1)
+print(text_2)
+print(text_3)
