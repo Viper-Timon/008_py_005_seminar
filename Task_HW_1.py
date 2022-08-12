9344995837a6d8c7c00e444737764ec9f841488f
viper-timon <viper-timon@mail.ru>
2022-08-12 14:26:36 +0300

    task1 universal try 1

--- Task_HW_1.py
+++ Task_HW_1.py
@@ -16,14 +16,16 @@
 import random as rnd
 import os
 
-cand_num = 2021
+min_st = 1  # мин ход - для универсальности решения
+max_st = 28  # макс ход - для универсальности решения
+cand_num = 2021  # для универсальности решения
 n = 0
 play_list = ['SkyNET']
 play_list.append(str(input('Введите свой nickname -> ')))
 os.system('cls')
 print(f'Против вас играет {play_list[0]}')
-#p_flag = rnd.randint(0, 1)
-p_flag = 0
+p_flag = rnd.randint(0, 1)
+p_flag = 1  # test
 
 print(f'Начинает игрок {play_list[p_flag]}')
 
@@ -32,29 +34,34 @@ while cand_num > 0:
     count = 0  # счетчик попыток игрока
 
     if p_flag == 0:
+        n = cand_num % (max_st+1)
+        if n == 0 or (min_st < n > max_st): 
+            n = rnd.randint(min_st, max_st)
 
-        n = cand_num % 29
-        if n == 0:
-            n = rnd.randint(1, 28)
-
-        cand_num = cand_num - n
         print(f'Игрок {play_list[p_flag]} берет {n} конфет')
         p_flag = 1
 
     else:
-        while not(0 < n < 29) == True:
-            n = int(input('Введите кол-во конфет от 1 до 28\n'))
-            count += 1
-            if count == 10:
-                n = cand_num % 29
-                if n == 0:
-                    n = rnd.randint(1, 28)
-                print(
-                    f'Игрок {play_list[not(p_flag)]} помогает игроку {play_list[p_flag]}\nи забирает {n} конфет')
+        while not((min_st-1) < n < (max_st + 1)):
 
-            cand_num = cand_num - n
+            n = int(input(f'Заберите от {min_st} до {max_st} конфет\n'))
+            count += 1
+
+            if count == 10:
+                n = cand_num % (max_st+1)
+                if n == 0 or (min_st < n > max_st):
+                    n = rnd.randint(min_st, max_st)
+                print(
+                    f'Игрок {play_list[not(p_flag)]} ' +
+                    f'помогает игроку {play_list[p_flag]}\nи подсказывает, что нужно забрать {n} конфет')
+
         p_flag = 0
+
+    cand_num = cand_num - n
+    n = 0
+
+    if cand_num < max_st:
+        max_st = cand_num
 
     if cand_num == 0:
         print(f'Победил игрок {play_list[not(p_flag)]}')
-    n = 0
